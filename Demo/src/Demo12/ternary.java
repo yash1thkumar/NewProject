9f3db3cb1dc0278f359195b5c65202a7e273f146
yash1thkumar <yashonethreddy@gmail.com>
2021-08-25 02:33:05 +0530

    "First commit and push"

--- Demo/src/Demo12/ternary.java
+++ Demo/src/Demo12/ternary.java
@@ -5,7 +5,6 @@ public class ternary
 
 	public static void main(String[] args)
 	{
-		
 		int a=20,b=30;
 		int max=(a>b)? a:b;
 		System.out.println("Among "+a+" & "+b+ " the biggest number is: "+max);
